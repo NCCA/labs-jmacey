7eee021b5a4ca1f1ce2c558361fec786833dd605
jmacey <jmacey@bournemouth.ac.uk>
2026-02-24 12:48:37 +0000

    finished Image class

--- Image/src/image/Image.py
+++ Image/src/image/Image.py
@@ -1,5 +1,9 @@
 from dataclasses import dataclass
 
+import numpy as np
+from PIL import Image as PILImage
+from PIL.Image import fromarray
+
 
 @dataclass
 class rgba:
@@ -26,9 +30,48 @@ class ImageAccessError(Exception):
 
 
 class Image:
-    def __init__(self, width, height):
+    def __init__(self, width: int, height: int, fill_colour=None):
+        """
+        Image class generated with width, height and optional colour if none found it will be white. 
+        """
         self._width = width
         self._height = height
+        fill = self._validate_rgba(fill_colour)
+        self._rgba_data = np.full((self._height, self._width, 4), fill, dtype=np.uint8)
+
+    def _validate_rgba(self, fill_colour):
+        match fill_colour:
+            case None:
+                return (255, 255, 255, 255)
+            case rgba(r, g, b, a):
+                return (r, g, b, a)
+            case (r, g, b):
+                return (r, g, b, 255)
+            case (r, g, b, a):
+                return (r, g, b, a)
+            case _:
+                raise TypeError(f"Invalid type for RGBA colour : {type(fill_colour).__name__}")
+
+    def _check_bounds(self, x, y):
+        if not (0 <= x < self._width and 0 <= y < self._height):
+            raise IndexError(f"x,y values out of range {x=} {self.width=} {y=} {self.height=}")
+
+    def get_pixel(self, x, y):
+        self._check_bounds(x, y)
+        return tuple(self._rgba_data[y, x])
+
+    def set_pixel(self, x, y, value):
+        self._check_bounds(x, y)
+        v = self._validate_rgba(value)
+        self._rgba_data[y, x] = v
+
+    def save(self, name):
+        img = PILImage.fromarray(self._rgba_data)
+        img.save(name)
+
+    def clear(self, colour):
+        c = self._validate_rgba(colour)
+        self._rgba_data[:] = c
 
     @property
     def height(self) -> int:
--- Image/tests/test_image.py
+++ Image/tests/test_image.py
@@ -51,3 +51,42 @@ def test_change_size(small_image):
         small_image.width = 100
     with pytest.raises(ImageAccessError):
         small_image.height = 100
+
+
+def test_image_red():
+    img = Image(2, 3, (255, 0, 0, 255))
+    for y in range(img.height):
+        for x in range(img.width):
+            r, g, b, a = img.get_pixel(x, y)
+            assert r == 255
+            assert g == 0
+            assert b == 0
+            assert a == 255
+
+
+def test_image_blue():
+    img = Image(2, 3, (0, 0, 255, 255))
+    for y in range(img.height):
+        for x in range(img.width):
+            r, g, b, a = img.get_pixel(x, y)
+            assert r == 0
+            assert g == 0
+            assert b == 255
+            assert a == 255
+
+
+def test_out_of_bounds():
+    img = Image(2, 2)
+    with pytest.raises(IndexError):
+        _ = img.get_pixel(2, 2)
+
+
+def test_set_pixel():
+    img = Image(3, 1)
+    img.set_pixel(0, 0, (255, 0, 0))
+    img.set_pixel(1, 0, (0, 255, 0))
+    img.set_pixel(2, 0, (0, 0, 255))
+    assert img.get_pixel(0, 0) == (255, 0, 0, 255)
+    assert img.get_pixel(1, 0) == (0, 255, 0, 255)
+    assert img.get_pixel(2, 0) == (0, 0, 255, 255)
+    img.save("rgb.png")
